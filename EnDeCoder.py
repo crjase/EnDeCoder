dfdf6f279c6d15e83b0e4d247f832a742d4f53a6
crjase <crjase@gmail.com>
2025-03-11 16:36:22 +1100

    Update EnDeCoder.py
    Made minor improvements

--- EnDeCoder.py
+++ EnDeCoder.py
@@ -3,26 +3,18 @@ import random
 
 
 
-#TODO change this data to individual characters instead of a=m, b=n, etc.
-char_data = {
+characters = [
     # a-z
-    'a': 'm', 'b': 'n', 'c': 'b', 'd': 'v', 'e': 'c', 'f': 'x',
-    'g': 'z', 'h': 'l', 'i': 'k', 'j': 'j', 'k': 'h', 'l': 'g',
-    'm': 'f', 'n': 'd', 'o': 's', 'p': 'a', 'q': 'p', 'r': 'o',
-    's': 'i', 't': 'u', 'u': 'y', 'v': 't', 'w': 'r', 'x': 'e',
-    'y': 'w', 'z': 'q',
-    
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+
     # A-Z
-    'A': 'M', 'B': 'N', 'C': 'B', 'D': 'V', 'E': 'C', 'F': 'X',
-    'G': 'Z', 'H': 'L', 'I': 'K', 'J': 'J', 'K': 'H', 'L': 'G',
-    'M': 'F', 'N': 'D', 'O': 'S', 'P': 'A', 'Q': 'P', 'R': 'O',
-    'S': 'I', 'T': 'U', 'U': 'Y', 'V': 'T', 'W': 'R', 'X': 'E',
-    'Y': 'W', 'Z': 'Q',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
 
     # 0-9
-    '1': '0', '2': '9', '3': '8', '4': '7', '5': '6', '6': '5',
-    '7': '4', '8': '3', '9': '2', '0': '1'
-}
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+]
 
 
 class EnDeCoder:
@@ -31,7 +23,7 @@ class EnDeCoder:
 
 
     def generate_key(self, length=32):
-        return ''.join(random.choices(list(char_data.keys()), k=length))
+        return ''.join(random.choices(characters, k=length))
 
 
     def encode(self, data, key):
@@ -56,17 +48,5 @@ class EnDeCoder:
 
 
 
-# Example
 if __name__ == "__main__":
-    endecoder = EnDeCoder()
-
-    data = "example_data"
-
-    key = endecoder.generate_key()
-    encoded_data = endecoder.encode(data, key)
-    decoded_data = endecoder.decode(encoded_data, key)
-
-    print(f"Data: {data}")
-    print(f"Key: {key}")
-    print(f"Encoded Data: {encoded_data}")
-    print(f"Decoded Data: {decoded_data}")
+    print("This is not an executable file. Please import this file in your project.")
